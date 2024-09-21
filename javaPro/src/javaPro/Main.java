f64c563852f42c73d6fa23b63b8218b17237fc24
Jimmy Master <mgsa112233@gmail.com>
2024-09-22 00:32:45 +0300

    JProgress Bar

--- javaPro/src/javaPro/Main.java
+++ javaPro/src/javaPro/Main.java
@@ -3,9 +3,8 @@ package javaPro;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//JSlider= gui component that lets user enter a value by using sliding knob on a track 
-		SliderDemo sliderDemo = new SliderDemo();
+		//prograss bar = visual aid to let the user know that an operatino is 
+		ProgressBar progressBar=new ProgressBar();
 	}
 
 }
