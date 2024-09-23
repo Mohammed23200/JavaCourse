e237a9722371e9a9927aa5e95656d86c1d553755
Jimmy Master <mgsa112233@gmail.com>
2024-09-23 22:41:08 +0300

    key listener

--- javaPro/src/javaPro/Main.java
+++ javaPro/src/javaPro/Main.java
@@ -3,7 +3,7 @@ package javaPro;
 public class Main {
 
 	public static void main(String[] args) {
-		//JColorChooser = A GUI machanism that let's user choose a color 
+		
 		MyFrame frame = new MyFrame();
 	}
 
--- javaPro/src/javaPro/MyFrame.java
+++ javaPro/src/javaPro/MyFrame.java
@@ -1,43 +1,51 @@
 package javaPro;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JColorChooser;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class MyFrame  extends  JFrame implements ActionListener{
+public class MyFrame  extends  JFrame implements KeyListener{
 
 JFrame frame = new  JFrame();
-JButton button;
 JLabel label;
 MyFrame(){
     this.setSize(500,500);
     this.setVisible(true);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLayout(new FlowLayout());
+    this.setLayout(null);
     this.pack();
-    button = new JButton("Pick a color ");
-    button.addActionListener(this);
+    this.addKeyListener(this);
     label = new JLabel();
+    label.setBounds(0, 0, 100, 100);
     label.setBackground(Color.white);
-    label.setText("this is some txt :D");
-    label.setFont(new Font("MV Boli",Font.PLAIN,100));
     label.setOpaque(true);
-    this.add(button);
-    this.add(label);
-} 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button){
-            JColorChooser colorChooser = new JColorChooser();
 
-            Color color = JColorChooser.showDialog(null, "pick a color", Color.BLACK);
-            label.setForeground(color);
-        }
 }
+@Override
+public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+    switch (e.getKeyChar()){
+        case 'a':label.setLocation(label.getX()-50,label.getY());
+        break;
+        case 'w':label.setLocation(label.getX(),label.getY()-50);
+        break;
+        case 's':label.setLocation(label.getX(),label.getY()+50);
+        break;
+        case 'd':label.setLocation(label.getX()+50,label.getY());
+
+    }
+}
+@Override
+public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+}
+@Override
+public void keyReleased(KeyEvent e) {
+    // TODO Auto-generated method stub
+   System.out.println("You released key char: "+e.getKeyChar());
+   System.out.println("You released key code: "+e.getKeyCode());
+} 
 }
