3533007aad2525ef5e3acc38f2b6830c07d38d86
Jimmy Master <mgsa112233@gmail.com>
2024-09-23 12:53:57 +0300

    file chooser

--- javaPro/src/javaPro/Main.java
+++ javaPro/src/javaPro/Main.java
@@ -3,6 +3,8 @@ package javaPro;
 public class Main {
 
 	public static void main(String[] args) {
+		//JFileChooser = A GUI machanism that let's user choose a file (helpful for open or save files )
+		
 		MyFrame frame = new MyFrame();
 	}
 
--- javaPro/src/javaPro/MyFrame.java
+++ javaPro/src/javaPro/MyFrame.java
@@ -3,64 +3,36 @@ package javaPro;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 
 public class MyFrame  extends  JFrame implements ActionListener{
-JFrame frame = new JFrame();
-JMenuItem loadItem;
-JMenuItem saveItem;
-JMenuItem exitItem;
-JMenu fileMenu;
-JMenu editMenu;
-JMenu helpMenu;
+
+JFrame frame = new  JFrame();
+JButton button;
 MyFrame(){
     this.setSize(500,500);
     this.setVisible(true);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new FlowLayout());
-
-    fileMenu = new JMenu("File");
-    editMenu = new JMenu("Edit");
-    helpMenu = new JMenu("Help");
-
-    loadItem = new JMenuItem("Load");
-    saveItem = new JMenuItem("Save");
-    exitItem = new JMenuItem("Exit");
-
-    loadItem.addActionListener(this);
-    saveItem.addActionListener(this);
-    exitItem.addActionListener(this);
-    loadItem.setMnemonic(KeyEvent.VK_L);//l for reloading
-    saveItem.setMnemonic(KeyEvent.VK_S);//S for saving
-    exitItem.setMnemonic(KeyEvent.VK_E);//E for exiting
-    fileMenu.setMnemonic(KeyEvent.VK_F);//l for file
-    fileMenu.add(loadItem);
-    fileMenu.add(saveItem);
-    fileMenu.add(exitItem);
-    JMenuBar menu = new JMenuBar();
-    this.setJMenuBar(menu);
-    menu.add(fileMenu);
-    menu.add(editMenu);
-    menu.add(helpMenu);
-    
+    button = new JButton("Select file");
+    button.addActionListener(this);
+    this.add(button);
 
 } 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==loadItem){
-            System.out.println("Loader");
+        if (e.getSource()==button){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));//set the current dirctory 
+            System.out.println(fileChooser.showOpenDialog(null));//select file to open and this return an integer value 
+            int response = fileChooser.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
         }
-        else if (e.getSource()==saveItem) {
-            System.out.println("you have saved");
-        }
-        else if (e.getSource()==exitItem){
-            System.exit(0);
-        }
-    }
-
-
+}
 }
